a369304049944fe7aa2f97e166b9f70e6b42b2f9
jheel-phantom <jhljheel@gmail.com>
2024-11-21 23:45:04 +0530

    added c#

---
+++ loops/c#/c#/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        int u = Int32.Parse(args[0]);
+        Random random = new Random();
+
+        int r = random.Next(0, 10000);
+
+        int[] a = new int[10000];
+
+        for (int i = 0; i < 10000; i++)
+        {
+            for (int j = 0; j < 10000; j++)
+            {
+                a[i] = a[i] + j % u;
+            }
+
+            a[i] += r;
+        }
+
+        Console.WriteLine(a[r]);
+    }
+}
+
+
+
---
+++ loops/c#/c#/readme.md
@@ -0,0 +1,3 @@
+Run the project using the following command:
+
+dotnet run -- 123   or any integer arguement

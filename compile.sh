1592513a9b45c15d2ad6e989a6b0b989030a1098
= <jhowell45@googlemail.com>
2024-11-22 14:55:39 +0000

    Moving the correct compiled cython files to the necessary dirs.

--- compile.sh
+++ compile.sh
@@ -7,8 +7,10 @@ go build -o go/code go/code.go
 # Cython
 python3 -m pip install cython setuptools
 python3 pyx/setup.py build_ext --inplace 
-mv code.cpython* build pyx/ -f
+mv *.cpython* build pyx/ -f
 
 # Java
 javac jvm/code.java
-RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release 
+
+# Rust
+# RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release 

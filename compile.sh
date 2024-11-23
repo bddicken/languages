5634079bd002e2c79d8eeff25354434c1bb199c9
Hari07 <22373191+Hari-07@users.noreply.github.com>
2024-11-23 16:15:42 +0400

    include dart

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@ clang -O3 c/code.c -o c/code
 go build -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release 
+dart compile exe dart/code.dart -o dart/code --target-os=macos

ea19071b1092221a7dc7b6e32311e38e17eefef3
Odonno <bottiau.david@laposte.net>
2024-11-25 22:21:56 +0100

    Add C# benchmarks

---
+++ fibonacci/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿int u = int.Parse(args[0]);
+int r = 0;
+
+for (int i = 0; i < u; i++)
+{
+    r += Fibonacci(i);
+}
+
+Console.WriteLine(r);
+return;
+
+static int Fibonacci(int n)
+{
+    return n switch
+    {
+        0 => 0,
+        1 => 1,
+        _ => Fibonacci(n - 1) + Fibonacci(n - 2)
+    };
+}
---
+++ loops/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿int u = int.Parse(args[0]);
+int r = Random.Shared.Next(0, 10000);
+int[] a = new int[10000];
+
+for (int i = 0; i < 10000; i++)
+{
+    for (int j = 0; j < 10000; j++)
+    {
+        a[i] += j % u;
+    }
+    a[i] += r;  
+}
+
+Console.WriteLine(a[r]);

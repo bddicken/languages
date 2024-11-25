7cb28ede321aefb4ac11e1087ac792a79b11c818
Thiago dos Santos <thiagodossantos@BRSAOMN044385.local>
2024-11-25 13:50:38 -0300

    kotlin jvm, js and native

--- compile.sh
+++ compile.sh
@@ -2,8 +2,6 @@ clang -O3 c/code.c -o c/code
 go build -ldflags "-s -w" -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
-kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
-#kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
 dart compile exe dart/code.dart -o dart/code --target-os=macos
 scala-cli --power package scala/code.scala -f -o scala/code
 ldc2 -O3 -release -boundscheck=off d/code.d
@@ -13,3 +11,30 @@ gfortran -O3 fortran/code.f90 -o fortan/code
 zig build-exe -O ReleaseFast -femit-bin=zig/code zig/code.zig
 luajit -b lua/code.lua lua/code
 swiftc -O -parse-as-library -Xcc -funroll-loops -Xcc -march=native -Xcc -ftree-vectorize -Xcc -ffast-math swift/code.swift -o swift/code
+kotlinc-jvm -include-runtime kotlin/jvm/code.kt -d kotlin/jvm/code.jar
+kotlinc-native kotlin/native/code.kt -o kotlin/native/code
+
+# Kotlin/JS workaround
+# https://youtrack.jetbrains.com/issue/KT-67089/K-JS-CLI-Compiling-.kt-.js-in-one-shot-is-not-possible
+KOTLIN_HOME= # it should point to Kotlin home directory
+BUILD_DIR=`pwd`/kotlin/js/build # it must be absolute path
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR/libs # there should be two separate directories for KLIBs and JS
+mkdir -p $BUILD_DIR/app
+
+## first, build KLIB
+$KOTLIN_HOME/bin/kotlinc-js \
+  -Xir-produce-klib-file \
+  -libraries $KOTLIN_HOME/lib/kotlin-stdlib-js.klib \
+  -ir-output-dir $BUILD_DIR/libs \
+  -ir-output-name code \
+  kotlin/js/code.kt
+
+## then, build JS
+$KOTLIN_HOME/bin/kotlinc-js \
+  -Xir-produce-js \
+  -libraries $KOTLIN_HOME/lib/kotlin-stdlib-js.klib \
+  -Xinclude=$BUILD_DIR/libs/code.klib \
+  -ir-output-dir $BUILD_DIR/app \
+  -ir-output-name code

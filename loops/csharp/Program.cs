0e43c127bedf7a511afed4c7514a03cfc5f849e2
Azrielx86 <edgar1569@outlook.com>
2024-11-25 22:05:18 -0600

    Added C# Loop benchmarks

---
+++ loops/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿var u = int.Parse(args[0]);
+var r = Random.Shared.Next(0, 10_000);
+var a = new int[10_000];
+
+for (var i = 0; i < 10_000; i++)
+{
+    for (int j = 0; j < 10_000; j++)
+    {
+        a[i] = a[i] + j % u;
+    }
+    a[i] += r;
+}
+
+Console.WriteLine(a[r]);

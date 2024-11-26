50a534c78437bc748e1ae3016830a7dc8a0ffa8a
Florian Osmani <florian.osmani@gmx.at>
2024-11-26 11:36:56 +0100

    SIMD C added

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 clang -O3 c/code.c -o c/code
+clang -mavx2 -O3 -mfma cfast/code.c -o cfast/code
 go build -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release

ea359959eabfee343e59cdc2338cbce8e81fa53e
Jonas Lara <adrianlr2515@outlook.com>
2024-11-26 16:04:50 -0600

    Add C# and F#

---
+++ loops/csharp/code.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int u = int.Parse(args[0]);               // Get an input number from the command line
+        Random random = new Random();
+        int r = random.Next(0, 10000);            // Get a random integer 0 <= r < 10k
+        int[] a = new int[10000];                // Array of 10k elements initialized to 0
+        for (int i = 0; i < 10000; i++)          // 10k outer loop iterations
+        {
+            for (int j = 0; j < 100000; j++)     // 100k inner loop iterations, per outer loop iteration
+            {
+                a[i] = a[i] + j % u;             // Simple sum
+            }
+            a[i] += r;                           // Add a random value to each element in array
+        }
+        Console.WriteLine(a[r]);                 // Print out a single element from the array
+    }
+}

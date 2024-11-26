b96ed9d371939089091e565f9868a59b40c44a49
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2024-11-26 17:24:29 +0100

    fixup! Made C, Go, Java, Kotlin, Python and Rust tailrecursive

--- fibonacci/kotlin/code.kt
+++ fibonacci/kotlin/code.kt
@@ -1,9 +1,12 @@
-fun fibonacci(n: Int): Int {
-    return fibonacciAccumulator(n,0,12)
-}
-fun fibbonaciAccumulator(n: Int, a: Int, b: Int): Int {
+fun fibonacci(n: Int): Int = fibonacciAccumulator(n, 0, 1)
+
+fun fibonacciAccumulator(
+    n: Int,
+    a: Int,
+    b: Int,
+): Int {
     if (n == 0) return a
-    return fibbonaciAccumulator(n-1, b, a+b)
+    return fibonacciAccumulator(n - 1, b, a + b)
 }
 
 fun main(args: Array<String>) {
@@ -14,4 +17,3 @@ fun main(args: Array<String>) {
     }
     println(r)
 }
-

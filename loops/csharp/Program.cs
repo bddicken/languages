4c3a25decee6230b334dd7ac29491337b910c687
Odonno <bottiau.david@laposte.net>
2024-11-26 18:54:48 +0100

    Use Span<int>

--- loops/csharp/Program.cs
+++ loops/csharp/Program.cs
@@ -1,10 +1,10 @@
 ﻿int u = int.Parse(args[0]);
-int r = Random.Shared.Next(0, 10000);
-int[] a = new int[10000];
+int r = Random.Shared.Next(0, 10_000);
+Span<int> a = stackalloc int[10_000];
 
-for (int i = 0; i < 10000; i++)
+for (int i = 0; i < 10_000; i++)
 {
-    for (int j = 0; j < 100000; j++)
+    for (int j = 0; j < 100_000; j++)
     {
         a[i] += j % u;
     }

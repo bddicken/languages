3097bcf9e4e376a19d30685d9687e116c756f757
xTrayambak <xtrayambak@disroot.org>
2024-11-26 10:00:37 +0530

    (add) add Nim to compile.sh

--- compile.sh
+++ compile.sh
@@ -6,6 +6,7 @@ native-image -O3 jvm.code
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
 cargo build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
+nim c --define:release --passC:"-O3" --define:speed --define:danger --out:nim/code nim/code.nim
 #kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
 dart compile exe dart/code.dart -o dart/code --target-os=macos
 sbcl --noinform --non-interactive --load "common-lisp/code.lisp" --build

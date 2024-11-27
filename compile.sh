880545d9d7b4c518bdd593d7741fab262973440e
A1029384756 <hayden.gray104@gmail.com>
2024-11-26 22:30:11 -0500

    added odin and made dart not platform-locked

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
 #kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
-dart compile exe dart/code.dart -o dart/code --target-os=macos
+dart compile exe dart/code.dart -o dart/code
+odin build odin -out:odin/code -o:speed

097b5e7d8179c5636477ac07b57898d19a001bf8
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-11-26 22:38:13 -0800

    Re-indent with swift-format.

--- fibonacci/swift/code.swift
+++ fibonacci/swift/code.swift
@@ -4,12 +4,12 @@ func fibonacci(n: Int) -> Int {
     if n < 2 {
         return n
     } else {
-        return fibonacci(n: n-1) + fibonacci(n: n-2)
+        return fibonacci(n: n - 1) + fibonacci(n: n - 2)
     }
 }
 
 func main() {
-    let u   = Int(CommandLine.arguments[1])!
+    let u = Int(CommandLine.arguments[1])!
     var sum = 0
     for i in 1..<u {
         sum += fibonacci(n: i)

3dce15ca3af8f369f19d956f5da4862bedaf75b1
Aparup Roychowdhury <aparuproychowdhury@gmail.com>
2024-11-27 18:15:18 +0530

    added cpp

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 clang -O3 c/code.c -o c/code
+clang++ cpp/code.cpp -o cpp/code
 go build -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
---
+++ fibonacci/cpp/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int32_t fibonacci(int32_t n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main (int argc, char* argv[]) {
+  int32_t u = atoi(argv[1]);
+  int32_t r = 0;
+  for (int32_t i = 1; i < u; i++) {
+    r += fibonacci(i);
+  }
+  std::cout<< r << std::endl;
+}

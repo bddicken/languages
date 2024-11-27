ae5cfceae8d7813291d5dc91d7232a75e0256d53
Jonas Lara <adrianlr2515@outlook.com>
2024-11-26 20:21:31 -0600

    Update C# and F# code, upgrade target framework to net9.0, and enable AOT publishing

--- loops/csharp/code.cs
+++ loops/csharp/code.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        int u = int.Parse(args[0]);               // Get an input number from the command line
+        var u = int.Parse(args[0]);               // Get an input number from the command line
         Random random = new Random();
-        int r = random.Next(0, 10000);            // Get a random integer 0 <= r < 10k
-        int[] a = new int[10000];                // Array of 10k elements initialized to 0
+        var r = random.Next(0, 10000);            // Get a random integer 0 <= r < 10k
+        var a = new int[10000];                // Array of 10k elements initialized to 0
         for (int i = 0; i < 10000; i++)          // 10k outer loop iterations
         {
             for (int j = 0; j < 100000; j++)     // 100k inner loop iterations, per outer loop iteration
--- loops/fsharp/code.fs
+++ loops/fsharp/code.fs
@@ -3,12 +3,11 @@
 [<EntryPoint>]
 let main args =
     let u = int args.[0] // Get an input number from the command line
-    let random = Random()
-    let r = random.Next(0, 10000) // Get a random integer 0 <= r < 10k
-    let a = Array.zeroCreate<int> 10000 // Array of 10k elements initialized to 0
+    let r = Random.Shared.Next(0, 10000) // Get a random integer 0 <= r < 10k
+    let a = Array.zeroCreate 10000 // Array of 10k elements initialized to 0
     for i in 0 .. 9999 do // 10k outer loop iterations
         for j in 0 .. 99999 do // 100k inner loop iterations, per outer loop iteration
             a.[i] <- a.[i] + j % u // Simple sum
         a.[i] <- a.[i] + r // Add a random value to each element in array
-    printfn "%d" a.[r] // Print out a single element from the array
+    Console.WriteLine(a.[r]) // Print out a single element from the array
     0 // Return exit code

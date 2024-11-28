2ceecd4b5a6beab0f5ebafd605553d27f255b0ee
LOSEARDES77 <loseardes77@gmail.com>
2024-11-28 09:05:23 +0100

    linix port

---
+++ default.nix
@@ -0,0 +1,32 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  name = "benchmark-env";
+
+  buildInputs = [
+    pkgs.clang # C compiler
+    pkgs.go # Go compiler
+    pkgs.openjdk # Java JDK (includes javac)
+    pkgs.rustup # Rustup for managing Rust toolchains
+    pkgs.kotlin # Kotlin compiler
+    pkgs.kotlin-native # Kotlin/Native compiler
+    pkgs.dart # Dart SDK
+    pkgs.nodejs # Node.js runtime
+    pkgs.bun # Bun runtime
+    pkgs.deno # Deno runtime
+    pkgs.pypy3 # PyPy3 interpreter
+    pkgs.python3 # Python3 interpreter
+    pkgs.ruby # Ruby interpreter
+    pkgs.php # PHP interpreter
+    pkgs.R # R interpreter
+    pkgs.time # GNU time command for timing execution
+    pkgs.gnumake # Make, useful for build scripts
+  ];
+
+  shellHook = ''
+    # Ensure Rust nightly is available
+    rustup install nightly
+    rustup default nightly
+
+    echo "Development environment is ready. All necessary tools are available."
+  '';
+}
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
+
 function runOnce  {
-  { /usr/bin/time $2 ; } 2> /tmp/o 1> /dev/null
+  { time $2 ; } 2> /tmp/o 1> /dev/null
   printf "$1 = "
-  cat /tmp/o | awk -v N=1 '{print $N"s"}'
+  cat /tmp/o | sed -n 's/real\s*\([0-9]*m\)\?\([0-9]*,[0-9]*\)s/\2/p' | tr ',' '.' | awk '{ print $1 }'
 }
 
 function run {
@@ -12,16 +13,16 @@ function run {
 }
 
 run "Kotlin" "java -jar kotlin/code.jar 40"
-run "C" "./c/code 40" 
-run "Go" "./go/code 40" 
+run "C" "./c/code 40"
+run "Go" "./go/code 40"
 run "Rust" "./rust/target/release/code 40"
-run "Node" "node ./js/code.js 40" 
-run "Bun" "bun ./js/code.js 40" 
-run "Deno" "deno ./js/code.js 40" 
-run "PyPy" "pypy ./py/code.py 40" 
+run "Node" "node ./js/code.js 40"
+run "Bun" "bun ./js/code.js 40"
+run "Deno" "deno ./js/code.js 40"
+run "PyPy" "pypy3 ./py/code.py 40"
 run "Java" "java jvm.code 40"
 run "Ruby" "ruby ./ruby/code.rb 40"
 run "PHP" "php ./php/code.php 40"
 run "R" "Rscript ./r/code.R 40"
-run "Python" "python3 ./py/code.py 40" 
+run "Python" "python3 ./py/code.py 40"
 run "Dart" "./dart/code 40"

f1c79e119e02fef40204d3d381dc1cb195d507b0
pjmlp <pjmlp@progtools.org>
2024-11-28 21:19:59 +0100

    Final touch on the D example.

--- loops/d/code.d
+++ loops/d/code.d
@@ -2,7 +2,7 @@ import std;
 
 void main(string[] args) {
   int u = to!int(args[1]);             // Get an input number from the command line
-  int r = rndGen().take(1) % 10000;              // Get a random integer 0 <= r < 10k
+  int r = uniform(0, 10000, rndGen) ;  // Get a random integer 0 <= r < 10k
   int[10000] a = 0;                    // Array of 10k elements initialized to 0
   for (int i = 0; i < 10000; i++) {    // 10k outer loop iterations
     for (int j = 0; j < 100000; j++) { // 100k inner loop iterations, per outer loop iteration

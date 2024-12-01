d75c4bc80239e7b7388a1531588443914dca55bb
Vlad Patryshev <vpatryshev@gmail.com>
2024-12-01 16:31:49 -0500

    typo

--- fibonacci/scala3/code.scala
+++ fibonacci/scala3/code.scala
@@ -2,6 +2,6 @@ def fibonacci(n: Int): Int =
   if (n < 2) n else fibonacci(n-1) + fibonacci(n-2)
 
 @main def code(n: Int): Unit =
-  println ((1 until s.toInt map fibonacci).sum)
+  println ((1 until n map fibonacci).sum)
 
 

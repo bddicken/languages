e9befb4f387eadacf5f8d7457ef703c168cfe874
LouisZAM <67562088+LouisZAM@users.noreply.github.com>
2024-12-04 10:41:46 -0800

    Update code.R
    Small optimization

--- loops/r/code.R
+++ loops/r/code.R
@@ -7,11 +7,12 @@ r <- sample(0:9999, 1)
 
 # Initialize an array of 10k elements to 0
 a <- rep(0, 10000)
-
+# avoid recreating sequence 
+seq_2 <- seq_len(100000) - 1
 # Perform the nested loop operation
 for (i in seq_len(10000)) {          # Outer loop: 10k iterations
-  num = 0 
-  for (j in seq_len(100000) - 1) {  # Inner loop: 100k iterations
+  num <- 0 
+  for (j in seq_2) {  # Inner loop: 100k iterations
     num <- num + (j %% u)         # Simple sum
   }
   a[i] <- num + r                  # Add the random value to each element

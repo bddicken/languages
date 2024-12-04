b2222b2d93de034e2e2190c424a67f7fa51eedf0
Avraham Adler <avraham.adler@mcgillpartners.com>
2024-12-04 19:20:37 +0200

    R-appropriate implementation

---
+++ loops/r/code2.R
@@ -0,0 +1,12 @@
+# Get input number from the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+u <- as.integer(args[1])  # Convert to integer
+
+# Generate a random number between 0 and 10000
+r <-  sample.int(10000, 1) - 1L
+
+# Vectored version of loop
+a <- rep(sum((seq_len(100000) - 1L) %% u), 10000) + r
+
+# Print a single element from the array
+print(a[r + 1])  # Adjusted for R's 1-based indexing

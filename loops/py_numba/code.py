22113c0c9897f51bb6b1070e36b3fc9fb78a3326
mzuber <michael.zuber@mlp.com>
2024-12-05 10:32:27 +0000

    Add implementation (for loops) using numba in python

---
+++ loops/py_numba/code.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import numba
+
+
+def main():
+    u = int(sys.argv[1])  # Get an input number from the command line
+    r = random.randint(0, 10000)  # Get a random number 0 <= r < 10k
+    a = main_numba(u, r)
+    print(a[r])  # Print out a single element from the array
+
+
+@numba.njit
+def main_numba(u, r):
+    a = [0] * 10_000  # Array of 10k elements initialized to 0
+    for i in range(10_000):  # 10k outer loop iterations
+        for j in range(100_000):  # 100k inner loop iterations, per outer loop iteration
+            a[i] += j % u  # Simple sum
+        a[i] += r  # Add a random value to each element in array
+    return a
+
+
+main()

3276d7076e430a901005e73ac2bcd74ebfdf04e2
Kainoa Kanter <kainoa@t1c.dev>
2024-12-06 09:32:40 -0800

    fish: fix loop logic

--- loops/fish/code.fish
+++ loops/fish/code.fish
@@ -2,9 +2,15 @@
 
 set -l u (math $argv[1])
 set -l r (math (random) % 10000)
-set -l a (seq 10000)
+set -l a
+for i in (seq 10000)
+    set -a a 0
+end
 
-for i in $a
+for i in (seq 10000)
+    for j in (seq 100000)
+        set a[$i] (math $a[$i] + $j % $u)
+    end
     set a[$i] (math $a[$i] + $r)
 end
 echo $a[$r]

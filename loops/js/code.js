043e60fe6c4918bb0cda63c63d0cb4e4d55e1958
Andrei Nedobylskii | 10101.art | Translay.Today <admin@twister-vl.ru>
2024-12-06 11:46:44 +0100

    Less array access in the loop and consts
    Using consts and additional var eliminates unnecessary accesses to the array.

--- loops/js/code.js
+++ loops/js/code.js
@@ -1,9 +1,10 @@
-var u = Number(process.argv[2]);           // Get an input number from the command line
-var r = Math.floor(Math.random() * 10000); // Get a random number 0 <= r < 10k
-var a = new Int32Array(10000);             // Array of 10k elements initialized to 0
+const u = Number(process.argv[2]);           // Get an input number from the command line
+const r = Math.floor(Math.random() * 10000); // Get a random number 0 <= r < 10k
+const a = new Int32Array(10000);             // Array of 10k elements initialized to 0
 for (let i = 0; i < 10000; i++) {          // 10k outer loop iterations
+  const t = a[i];
   for (let j = 0; j < 100000; j++) {       // 100k inner loop iterations, per outer loop iteration
-    a[i] = a[i] + j%u;                     // Simple sum
+    a[i] = t + j%u;                     // Simple sum
   }
   a[i] += r;                               // Add a random value to each element in array
 }

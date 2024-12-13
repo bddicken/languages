358d55159f9dbdc4d140865ba1cd9de39abe94ae
Kayden <143221653+Kayden1412@users.noreply.github.com>
2024-12-13 12:32:56 +0700

    Use biased rng

--- loops/zig/code.zig
+++ loops/zig/code.zig
@@ -10,7 +10,7 @@ pub fn main() !void {
     const u = try std.fmt.parseInt(usize, arg, 10);
 
     // Get a random number 0 <= r < 10k
-    const r = rand.intRangeAtMost(usize, 0, 10000);
+    const r = rand.uintLessThanBiased(usize, 10000);
 
     // Array of 10k elements initialized to 0
     var a: [10000]usize = undefined;

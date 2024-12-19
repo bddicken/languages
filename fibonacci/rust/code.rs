ff769950da209b1799690c46297fa5689eb38c17
Daniel <danieloraca@users.noreply.github.com>
2024-12-19 18:21:40 +0000

    perf: improve performance of rust fibonacci code

--- fibonacci/rust/code.rs
+++ fibonacci/rust/code.rs
@@ -1,20 +1,21 @@
 fn fibonacci(n: u32) -> u32 {
-    match n {
-        0 => 0,
-        1 => 1,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..n {
+        let temp = a;
+        a = b;
+        b = temp + b;
     }
+    a
 }
 
 fn main() {
-    let u: u32 = std::env::args()
+    let input = std::env::args()
         .nth(1)
-        .unwrap()
-        .parse()
-        .unwrap();
-    let mut r = 0;
-    for i in 1..u {
-        r += fibonacci(i);
-    }
-    println!("{}", r);
+        .expect("Please provide a number as an argument");
+    let u: u32 = input.parse().expect("Failed to parse input as a number");
+
+    let sum: u32 = (0..u).map(fibonacci).sum();
+
+    println!("{}", sum);
 }

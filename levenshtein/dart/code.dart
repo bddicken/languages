8f2059eff03e055369c9c9dd58fc28f54f904379
vvarga <vvarga@clickup.com>
2024-12-19 12:00:07 +0100

    Extract row and prev row in levenshtein dist calculation

--- levenshtein/dart/code.dart
+++ levenshtein/dart/code.dart
@@ -21,15 +21,17 @@ int levenshteinDistance(String word1, String word2) {
 
   // Compute Levenshtein distance
   for (int i = 1; i <= m; i++) {
+    final row = matrix[i];
+    final previousRow = matrix[i - 1];
     for (int j = 1; j <= n; j++) {
       int cost = word1[i - 1] == word2[j - 1] ? 0 : 1;
-      matrix[i][j] = min(
-          min(
-              matrix[i - 1][j] + 1, // Deletion
-              matrix[i][j - 1] + 1 // Insertion
-              ),
-          matrix[i - 1][j - 1] + cost // Substitution
-          );
+      row[j] = min(
+        min(
+          previousRow[j] + 1, // Deletion
+          row[j - 1] + 1, // Insertion
+        ),
+        previousRow[j - 1] + cost, // Substitution
+      );
     }
   }
   return matrix[m][n];

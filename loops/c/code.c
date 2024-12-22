e84704c1fd9694f30fcca035fd95fd7ae1ea8c70
JustForCollege <sim.AhmedMahmoud38270@alexu.edu.eg>
2024-12-22 07:15:22 +0200

    Update code.c

--- loops/c/code.c
+++ loops/c/code.c
@@ -3,6 +3,9 @@
 #include "stdint.h"
 #include "time.h"
 
+// A casey inspired macro
+#define REM(a, b) (long long)(((double)a) - ((double)b)*trunc(((double)a) / ((double)b)))
+
 int main (int argc, char** argv) {
   int u = atoi(argv[1]);               // Get an input number from the command line
   srand(time(NULL));                   // FIX random seed
@@ -10,7 +13,7 @@ int main (int argc, char** argv) {
   int32_t a[10000] = {0};              // Array of 10k elements initialized to 0
   for (int i = 0; i < 10000; i++) {    // 10k outer loop iterations
     for (int j = 0; j < 100000; j++) { // 100k inner loop iterations, per outer loop iteration
-      a[i] = a[i] + j%u;               // Simple sum
+      a[i] = a[i] + REM(j,u)               // Simple sum
     }
     a[i] += r;                         // Add a random value to each element in array
   }

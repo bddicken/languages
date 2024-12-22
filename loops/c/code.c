2c78252f445256b210337b49a922c70071d0f945
Saïd <20957603+ayewo@users.noreply.github.com>
2024-12-22 21:29:23 +0100

    trigger SIMD compiler optimizations
    via @cmuratori on YT https://youtu.be/RrHGX1wwSYM?t=3805

--- loops/c/code.c
+++ loops/c/code.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "stdint.h"
 #include "time.h"
+#include "math.h"
 
 int main (int argc, char** argv) {
   int u = atoi(argv[1]);               // Get an input number from the command line
@@ -10,7 +11,10 @@ int main (int argc, char** argv) {
   int32_t a[10000] = {0};              // Array of 10k elements initialized to 0
   for (int i = 0; i < 10000; i++) {    // 10k outer loop iterations
     for (int j = 0; j < 100000; j++) { // 100k inner loop iterations, per outer loop iteration
-      a[i] = a[i] + j%u;               // Simple sum
+      double quotient = (double)j / (double)u;
+	    quotient = trunc(quotient);
+	    double remainder = (double)j - (double)u*quotient;
+	    a[i] = a[i] + (int)remainder;
     }
     a[i] += r;                         // Add a random value to each element in array
   }

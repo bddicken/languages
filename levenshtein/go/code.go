95d0d0f03374ff0e0a612b204293bc1496a0378b
zveinn <info@sveinn.is>
2025-01-12 22:45:53 +0000

    Just adding an else to the if gets you about 30% performance

--- levenshtein/go/code.go
+++ levenshtein/go/code.go
@@ -9,17 +9,6 @@ import (
 // Space Complexity: O(min(m,n)) - only uses two rows instead of full matrix
 // Time Complexity: O(m*n) where m and n are the lengths of the input strings
 func levenshtein(s1, s2 string) int {
-	// Early termination checks
-	if s1 == s2 {
-		return 0
-	}
-	if len(s1) == 0 {
-		return len(s2)
-	}
-	if len(s2) == 0 {
-		return len(s1)
-	}
-
 	// Make s1 the shorter string for space optimization
 	if len(s1) > len(s2) {
 		s1, s2 = s2, s1
@@ -37,26 +26,21 @@ func levenshtein(s1, s2 string) int {
 	}
 
 	// Main computation loop
+	var cost int = 1
 	for j := 1; j <= n; j++ {
 		currRow[0] = j
 
 		for i := 1; i <= m; i++ {
-			cost := 1
+			cost = 1
 			if s1[i-1] == s2[j-1] {
 				cost = 0
 			}
-			
-			// Find minimum of three operations
-			deletion := prevRow[i] + 1
-			insertion := currRow[i-1] + 1
-			substitution := prevRow[i-1] + cost
-			
-			currRow[i] = deletion
-			if insertion < currRow[i] {
-				currRow[i] = insertion
-			}
-			if substitution < currRow[i] {
-				currRow[i] = substitution
+
+			currRow[i] = prevRow[i] + 1
+			if currRow[i-1]+1 < currRow[i] {
+				currRow[i] = currRow[i-1] + 1
+			} else if prevRow[i-1]+cost < currRow[i] {
+				currRow[i] = prevRow[i-1] + cost
 			}
 		}
 
@@ -78,15 +62,24 @@ func main() {
 		return
 	}
 
+	var d int
 	for i := 0; i < len(args); i++ {
 		for j := 0; j < len(args); j++ {
-			if  i != j {
-        d := levenshtein(args[i], args[j])
-        if minD == -1 || d < minD {
-          minD = d
-        }
-        times++
-      }
+			if i != j {
+				if len(args[i]) == 0 {
+					d = len(args[j])
+				} else if len(args[j]) == 0 {
+					d = len(args[i])
+				} else if args[i] == args[j] {
+					d = 0
+				} else {
+					d = levenshtein(args[i], args[j])
+				}
+				if minD == -1 || d < minD {
+					minD = d
+				}
+				times++
+			}
 		}
 	}
 
